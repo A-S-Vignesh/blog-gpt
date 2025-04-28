59ca3c51244acd13dcb3c62ba251acfe63f7fd37
A-S-Vignesh <asvicki2002@gmail.com>
2025-04-28 16:21:02 +0530

    Update user.js

--- db/models/user.js
+++ db/models/user.js
@@ -3,15 +3,15 @@ import { model, models, Schema } from "mongoose";
 const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, "emailId already exists!"],
-    required: [true, "emailId is required!"],
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
   },
   username: {
     type: String,
-    required: [true, "username is required!"],
+    required: [true, "Username is required!"],
     match: [
       /^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+      "Username invalid, it should be 6-20 characters, alphanumeric and unique!",
     ],
   },
   image: {
@@ -19,12 +19,6 @@ const UserSchema = new Schema({
   },
 });
 
-// Ensure the model is only registered once
-let User;
-try {
-  User = model("User", UserSchema);
-} catch (error) {
-  User = models.User;
-}
+const User = models.User || model("User", UserSchema);
 
 export default User;
