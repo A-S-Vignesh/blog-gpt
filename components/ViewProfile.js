8f9aa496e09c7c96fb4355f71230f2492238acc6
A-S-Vignesh <asvicki2002@gmail.com>
2025-07-14 18:47:35 +0530

    build error correct

--- components/ViewProfile.js
+++ components/ViewProfile.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import BlogPost from "./BlogPost";
-import Loading from "@/app/loading";
+// import Loading from "@/app/loading";
 import { InfinitySpin } from "react-loader-spinner";
 import LoadingSkeleton from "@/components/LoadingSkeleton";
 
@@ -106,7 +106,6 @@ const ViewProfile = ({ isMyProfile, userData, userPosts, error }) => {
 
   return (
     <section className="padding min-h-screen px-6 sm:px-16 md:px-20 lg:px-28 py-3 sm:py-4 bg-white dark:bg-dark-100">
-      {loading && <Loading />}
       <h2 className="title_heading">Profile</h2>
       <p className="text-lg text-slate-500 mt-0">
         Welcome to {isMyProfile ? "Your Personalized " : ""} Profile Page
