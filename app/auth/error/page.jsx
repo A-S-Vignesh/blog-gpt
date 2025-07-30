fbb7c0d9ca375f1a971694a7a1653b23de5e5db1
A-S-Vignesh <asvicki2002@gmail.com>
2025-07-30 23:17:48 +0530

    deployment error

--- app/auth/error/page.jsx
+++ app/auth/error/page.jsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+
 import Link from "next/link";
 
 export default function Error() {
-  const searchParams = useSearchParams();
-  const error = searchParams.get("error");
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <div className="flex flex-col items-center mt-10 p-10">
         <h1 className="text-4xl font-bold mb-8">Authentication Error</h1>
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
-          {error || "An error occurred during authentication"}
+          { "An error occurred during authentication"}
         </div>
         <Link href="/" className="black_btn mt-4">
           Return Home
